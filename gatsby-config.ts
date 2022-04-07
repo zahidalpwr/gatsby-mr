23e59fdba13ef9861c4a4fcb46f3da25a462980c
Zahid <zahid.alp@toptal.com>
2022-04-07 08:20:10 +0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Test Gatby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
