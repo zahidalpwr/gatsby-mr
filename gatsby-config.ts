d57d2b6adda72a3ba6a315768cbf4d823c3de631
Zahid <zahid.alp@toptal.com>
2022-04-17 13:17:05 +0300

    Plugins - gastby-plugin-image as a sample

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,10 @@ const config: GatsbyConfig = {
     title: `Test Gatby`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
 
 export default config
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 // Step 1: Import React
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react'
 import Layout from "../components/layout";
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-      <Layout pageTitle="Home Page">
-          <p>I'm making this by following the Gatsby Tutorial.</p>
-      </Layout>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/dog.webp"
+      />
+    </Layout>
   )
 }
 
